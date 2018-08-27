c94005c86f3e167cd9a2e97c48e210f5abe03581
kappalucky <kappalucky@aol.com>
2018-08-27 03:34:05 -0400

    added lodash and fixed styling

--- src/components/CurrencyTable.vue
+++ src/components/CurrencyTable.vue
@@ -14,7 +14,7 @@
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="coin in coins.data" :key="coin.id">
+                <tr v-for="coin in coins.data" :key="coin.rank">
                     <td>{{ coin.rank }}</td>
                     <td>
                         <!--<img v-bind:src="getCoinImage(coin.symbol)"-->{{ coin.name }}</td>
@@ -43,57 +43,59 @@ let COINMARKETCAP_API_URI = "https://api.coinmarketcap.com";
 let UPDATE_INTERVAL = 60 * 1000;
 
 export default {
-  name: "CurrencyTable",
-  data: function() {
-    return {
-      coins: [],
-      coinData: {}
-    };
-  },
-  created() {
-    this.getCoins();
-    this.getCoinData();
-  },
-  /*updated() {
-                                                          setInterval(() => {
-                                                            this.getCoins();
-                                                          }, UPDATE_INTERVAL);
-                                                        },*/
-  methods: {
-    // Loads currency data. Data used to find logos for each currency.
-    getCoinData: function() {
-      axios
-        .get(CRYPTOCOMPARE_API_URI + "/data/all/coinlist")
-        .then(response => {
-          this.coinData = response.data.Data;
-        })
-        .catch(err => {
-          console.error(err);
-        });
+    name: "CurrencyTable",
+    data: function () {
+        return {
+            coins: [],
+            coinData: {}
+        };
     },
-    // Get top 10 cryptocurrencies by value.
-    getCoins: function() {
-      axios
-        .get(COINMARKETCAP_API_URI + "/v2/ticker/?limit=10")
-        .then(response => {
-          console.log("request successful");
-          this.coins = response.data;
-        })
-        .catch(err => {
-          console.error(err);
-        });
+    created() {
+        this.getCoins();
+        this.getCoinData();
+        setInterval(() => {
+            this.getCoins();
+        }, UPDATE_INTERVAL);
     },
-    // Given cryptocurrency ticket symbol, return currency logo image.
-    getCoinImage: function(symbol) {
-      return "https://www.cryptocompare.com" + this.coinData[symbol].ImageUrl;
-    },
-    //return a css color depending on value passed, red - negative, green - positive
-    getColor: function(percentage) {
-      return percentage > 0 ? "color:green;" : "color:red;";
+    methods: {
+        // Loads currency data. Data used to find logos for each currency.
+        getCoinData: function () {
+            axios
+                .get(CRYPTOCOMPARE_API_URI + "/data/all/coinlist")
+                .then(response => {
+                    this.coinData = response.data.Data;
+                })
+                .catch(err => {
+                    console.error(err);
+                });
+        },
+        // Get top 10 cryptocurrencies by value.
+        getCoins: function () {
+            axios
+                .get(COINMARKETCAP_API_URI + "/v2/ticker/?limit=10")
+                .then(response => {
+                    console.log("request successful");
+                    this.coins = response.data;
+                })
+                .catch(err => {
+                    console.error(err);
+                });
+        },
+        // Given cryptocurrency ticket symbol, return currency logo image.
+        getCoinImage: function (symbol) {
+            return "https://www.cryptocompare.com" + this.coinData[symbol].ImageUrl;
+        },
+        //return a css color depending on value passed, red - negative, green - positive
+        getColor: function (percentage) {
+            return percentage > 0 ? "color:green;" : "color:red;";
+        }
     }
-  }
 };
 </script>
 
 <style scoped>
+table {
+  width: 100%;
+  table-layout: auto;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from "vue";
+import lodash from "lodash";
 import Vue2Filters from "vue2-filters";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-Vue.use(Vue2Filters);
+Vue.use(Vue2Filters, lodash);
 
 Vue.config.productionTip = false;
 
